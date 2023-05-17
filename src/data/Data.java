b3f2af35ea5827121d081d17900ece809ce067c1
KoteiMadoshi <antoniocatanzaro123@gmail.com>
2023-05-17 17:21:29 +0200

    Revert "Merge branch 'main' into Antonio"
    This reverts commit efd9e4623f986ded48acd23ed34c1cb99493c76b, reversing
    changes made to eb2ea0cfa877a253ab503633f33a5e70a11a30dc.

--- src/data/Data.java
+++ src/data/Data.java
@@ -63,26 +63,17 @@ public class Data {
 			Iterator<Object> exIterator = ex.example.iterator();
 			String thisString;
 			String exString;
-			Double thisDouble;
-			Double exDouble;
 
 			for(Object thisIterator : this.example){
-				if(thisIterator instanceof String){
-					thisString = (String)thisIterator;
-					exString = (String)exIterator.next();
-					if(!thisString.equals(exString)){
-						return  (thisString.compareTo(exString) > 0) ?  1 : -1;
-					}
-				} else if(thisIterator instanceof Double){
-					thisDouble = (Double)thisIterator;
-					exDouble = (Double)exIterator.next();
-					if(!thisDouble.equals(exDouble)){
-						return  (thisDouble.compareTo(exDouble) > 0) ?  1 : -1;
-					}
+				thisString = (String)thisIterator;
+				exString = (String)exIterator.next();
+				if(!thisString.equals(exString)){
+					int notEqual = (thisString.compareTo(exString) > 0) ?  1 : -1;
+					return notEqual;
 				}
 			}
-			return 0;
 
+			return 0;
 		}
 
 		/**
@@ -162,20 +153,20 @@ public class Data {
 		ex12.add(new String ("overcast"));
 		ex13.add(new String ("rain"));
 
-		ex0.add(Double.valueOf("37.5"));
-		ex1.add(Double.valueOf("38.7"));
-		ex2.add(Double.valueOf("37.5"));
-		ex3.add(Double.valueOf("20.5"));
-		ex4.add(Double.valueOf("20.7"));
-		ex5.add(Double.valueOf("21.2"));
-		ex6.add(Double.valueOf("20.5"));
-		ex7.add(Double.valueOf("21.2"));
-		ex8.add(Double.valueOf("21.2"));
-		ex9.add(Double.valueOf("19.8"));
-		ex10.add(Double.valueOf("3.5"));
-		ex11.add(Double.valueOf("3.6"));
-		ex12.add(Double.valueOf("3.5"));
-		ex13.add(Double.valueOf("3.2"));
+		ex0.add(new String ("hot"));
+		ex1.add(new String ("hot"));
+		ex2.add(new String ("hot"));
+		ex3.add(new String ("mild"));
+		ex4.add(new String ("cool"));
+		ex5.add(new String ("cool"));
+		ex6.add(new String ("cool"));
+		ex7.add(new String ("mild"));
+		ex8.add(new String ("cool"));
+		ex9.add(new String ("mild"));
+		ex10.add(new String ("mild"));
+		ex11.add(new String ("mild"));
+		ex12.add(new String ("hot"));
+		ex13.add(new String ("mild"));
 
 		ex0.add(new String ("high"));
 		ex1.add(new String ("high"));
@@ -257,7 +248,7 @@ public class Data {
 		temperatureValues[0]="hot";
 		temperatureValues[1]="mild";
 		temperatureValues[2]="cool";
-		attributeSet.add(new ContinuousAttribute("Temperature",1, 3.2, 38.7));
+		attributeSet.add(new DiscreteAttribute("Temperature", 1, temperatureValues));
 
 		//Humidity
 		String humidityValues[] = new String[2];
@@ -352,12 +343,7 @@ public class Data {
 		Tuple tuple = new Tuple(attributeSet.size());
 
 		for(Attribute iterAttr : attributeSet) {
-			if(iterAttr instanceof DiscreteAttribute){
-				tuple.add(new DiscreteItem((DiscreteAttribute)iterAttr, (String)data.get(index).get(iterAttr.getIndex())), iterAttr.getIndex());
-			} else if(iterAttr instanceof ContinuousAttribute){
-				tuple.add(new ContinuousItem((ContinuousAttribute)iterAttr, (Double)data.get(index).get(iterAttr.getIndex())), iterAttr.getIndex());
-			}
-
+			tuple.add(new DiscreteItem((DiscreteAttribute)iterAttr, (String)data.get(index).get(iterAttr.getIndex())), iterAttr.getIndex());
 		}
 
 		return tuple;
@@ -423,11 +409,7 @@ public class Data {
 	 * @return an object of the Object class, representing the centroid value with respect to the attribute.
 	 */
 	Object computePrototype(Set<Integer> idList, Attribute attribute){
-		if(attribute instanceof DiscreteAttribute){
-			return computePrototype(idList, (DiscreteAttribute)attribute);
-		}else {
-			return computePrototype(idList, (ContinuousAttribute)attribute);
-		}
+		return computePrototype(idList, (DiscreteAttribute)attribute);
 	}
 
 	/**
@@ -455,14 +437,4 @@ public class Data {
 		return stringToReturn;
 	}
 
-	private Double computePrototype(Set<Integer> idList, ContinuousAttribute attribute){
-		double avgValue = 0.0;
-
-		for(Integer iterIdList : idList){
-			avgValue += (Double)(data.get(iterIdList).get(attribute.getIndex()));
-		}
-		avgValue /= idList.size();
-
-		return avgValue;
-	}
 }
